b9adfcad1765b180958fa1a96b8fb317cb609abd
Andrey Sokolov <falconandy@yandex.ru>
2021-10-07 19:09:30 +0400

    Try 1 - symlink

--- Formula/ilpma.rb
+++ Formula/ilpma.rb
@@ -10,7 +10,6 @@ class Ilpma < Formula
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install_symlink libexec"/bin/ilpma" => "ilpma"
   end
 end
-
