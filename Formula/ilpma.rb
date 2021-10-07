c7bef91bb8fae28b8b65ba4ba78e48408afbea83
Andrey Sokolov <falconandy@yandex.ru>
2021-10-07 18:30:59 +0400

    Try 1

---
+++ Formula/ilpma.rb
@@ -0,0 +1,16 @@
+require "language/node"
+
+class Foo < Formula
+  desc "..."
+  homepage "https://example.com"
+  url "https://registry.npmjs.org/@falconandy/ilpma/-/ilpma-0.1.18.tgz"
+  sha256 "9fb0f17d9d378fc8be5f7d825c34e45a3e090e0e"
+
+  depends_on "node@12"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+end
+
