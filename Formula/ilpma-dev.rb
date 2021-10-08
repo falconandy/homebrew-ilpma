ad48a534b9996cd5ecb438be544127418c2412ac
Andrey Sokolov <falconandy@yandex.ru>
2021-10-08 10:11:57 +0400

    Try 2 - dev

--- Formula/ilpma-dev.rb
+++ Formula/ilpma-dev.rb
@@ -4,13 +4,14 @@ class IlpmaDev < Formula
   desc "..."
   homepage "https://example.com"
   url "https://github.com/falconandy/ilpma/releases/download/latest/ilpma-v0.1.19-master-08dfd095.0.tar.gz"
+  version "0.1.19-master-08dfd095.0"
   sha256 "facc2b78b81d42ddb8caeb1ac0272158dc44a51854894667b28412e68c2d3868"
 
   depends_on "node@12"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/ilpma" => "ilpma-dev"
   end
 
   test do
