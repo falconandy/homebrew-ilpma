9e4cca577ce1c7749e386aa6b270d5276287afc9
Andrey Sokolov <falconandy@yandex.ru>
2021-10-08 12:27:18 +0400

    Try 2 - dev

--- Formula/ilpma-dev.rb
+++ Formula/ilpma-dev.rb
@@ -4,7 +4,7 @@ class IlpmaDev < Formula
   desc "..."
   homepage "https://example.com"
   url "https://github.com/falconandy/ilpma/releases/download/latest/ilpma-v0.1.19-master-19afddc6.0.tar.gz"
-  version "0.1.19-master-19afddc6.0"
+  version "0.1.19-master-012.0"
   sha256 "9c4c8dde9f76aaf87878db0536cbd4ef15b6e6310a7b616bbc46a5ead1ec44b4"
 
   depends_on "node@12"
