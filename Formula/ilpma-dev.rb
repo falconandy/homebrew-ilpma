a18849ac584457318b4ccae0f4a44012c375fdd3
Andrey Sokolov <falconandy@yandex.ru>
2021-10-09 18:05:35 +0400

    Try 3

--- Formula/ilpma-dev.rb
+++ Formula/ilpma-dev.rb
@@ -1,10 +1,10 @@
 require "language/node"
 
 class IlpmaDev < Formula
-  desc "..."
+  desc "The Ilpma CLI (Dev)"
   homepage "https://example.com"
   url "https://github.com/falconandy/ilpma/releases/download/latest/ilpma-v0.1.19-master-19afddc6.0.tar.gz"
-  version "0.1.12"
+  version "0.1.19"
   sha256 "9c4c8dde9f76aaf87878db0536cbd4ef15b6e6310a7b616bbc46a5ead1ec44b4"
 
   depends_on "node@12"
--- Formula/ilpma.rb
+++ Formula/ilpma.rb
@@ -1,9 +1,10 @@
 require "language/node"
 
 class Ilpma < Formula
-  desc "..."
+  desc "The Ilpma CLI"
   homepage "https://example.com"
   url "https://registry.npmjs.org/@falconandy/ilpma/-/ilpma-0.1.18.tgz"
+  version "0.1.18"
   sha256 "8abfcf5a430fbe9bf2ac8a5ad26dd1435160790c01253574683b06ac9e8099c4"
 
   depends_on "node@12"
