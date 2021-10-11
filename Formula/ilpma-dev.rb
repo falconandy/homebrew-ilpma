c61f7113adb803c7fdcff8d12d782fbf3bc13951
falconandy <falconandy@users.noreply.github.com>
2021-10-11 17:33:04 +0000

    v0.1.31-master-20211011173239.0

--- Formula/ilpma-dev.rb
+++ Formula/ilpma-dev.rb
@@ -1,9 +1,9 @@
 class IlpmaDev < Formula
   desc "The Ilpma CLI (Dev)"
   homepage "https://example.com"
-  url "https://github.com/falconandy/ilpma/releases/download/latest/ilpma-v0.1.31-master-20211010063630.0.tar.gz"
-  version "0.1.31-master-20211010063630.0"
-  sha256 "f82c07895aec14d2e365c5ae13915aa52e1b2f168a093b2ccc05fda4dd2432b5"
+  url "https://github.com/falconandy/homebrew-ilpma/releases/download/latest/ilpma-v0.1.31-master-20211011173239.0.tar.gz"
+  version "0.1.31-master-20211011173239.0"
+  sha256 "317d0c9b2da352a451d2f99b8396052c89df1e36b1c6a5d2995434c7b113170a"
 
   depends_on "node@12"
 
