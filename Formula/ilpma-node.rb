ac69d237ab826814fd0e5dce269b89a1186d8e4a
Andrey Sokolov <falconandy@yandex.ru>
2021-10-19 09:22:46 +0400

    Custom Node

--- Formula/ilpma-node.rb
+++ Formula/ilpma-node.rb
@@ -3,6 +3,7 @@ class IlpmaNode < Formula
   homepage "https://example.com"
   url "https://file.io/shzX3eR3c2RU"
   sha256 "45d47a444f992250c2d0beeedc4884838a2d8bef20b1b4f78cb5ffc1886ace66"
+  version "12.22.7"
 
   def install
     libexec.install buildpath/"node"
