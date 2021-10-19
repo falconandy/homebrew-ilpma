642a8cabcec0166c9f160f13c7b5ad1884d2d9c4
Andrey Sokolov <falconandy@yandex.ru>
2021-10-19 09:18:03 +0400

    Custom Node

--- Formula/ilpma-node.rb
+++ Formula/ilpma-node.rb
@@ -1,7 +1,7 @@
 class IlpmaNode < Formula
   desc "node.js dependency for Ilpma CLI"
   homepage "https://example.com"
-  url "https://www.dropbox.com/s/qmo70wnohbz3kts/node-12.22.7.tar.xz?dl=1"
+  url "https://www.dropbox.com/s/dl/qmo70wnohbz3kts/node-12.22.7.tar.xz"
   sha256 "45d47a444f992250c2d0beeedc4884838a2d8bef20b1b4f78cb5ffc1886ace66"
 
   def install
