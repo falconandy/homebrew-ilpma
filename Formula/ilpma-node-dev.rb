16f834a29a1be31f61bd85dee3ebd8c90d273808
Andrey Sokolov <falconandy@yandex.ru>
2021-10-19 09:28:35 +0400

    Custom Node

--- Formula/ilpma-node-dev.rb
+++ Formula/ilpma-node-dev.rb
@@ -1,9 +1,9 @@
 class IlpmaNodeDev < Formula
   desc "node.js dependency for Ilpma CLI (Dev)"
   homepage "https://example.com"
-  url "https://file.io/AiWBbPgjVGLR"
-  sha256 "20336250cfcfe3e84bcbea457f0aa5f325eb020f3a86e972d36d9ab0e331ece0"
-  version "12.22.8"
+  url "https://github.com/falconandy/homebrew-ilpma/releases/download/v0.1.34/node-12.22.7.tar.xz"
+  sha256 "45d47a444f992250c2d0beeedc4884838a2d8bef20b1b4f78cb5ffc1886ace66"
+  version "12.22.9"
 
   def install
     libexec.install buildpath/"node"
